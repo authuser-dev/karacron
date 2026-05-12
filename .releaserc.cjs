765614acf7f6f694e39942835dd587a0a9475147
sergio.gonzalez <sgonzalez@authuser.org>
2026-05-12 15:12:02 +0200

    refactor: format release configuration

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -22,7 +22,8 @@ module.exports = {
 			"@semantic-release/git",
 			{
 				assets: ["CHANGELOG.md"],
-				message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+				message:
+					"chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
 			},
 		],
 	],
